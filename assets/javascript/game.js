f00102b267a84c04b61a2cbbb141f8b2bbb66814
Tony <Naplex@Tonys-MacBook.local>
2018-03-11 01:32:55 -0800

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,18 @@
 // create a list of words
-var colors = ["red", "green", "blue", "orange", "purple", "yellow"];
+var colors = ["red", "green", "blue", "orange", "purple", "yellow", "gray", "violet", "turquoise", "magenta", "cyan", "brown", "peach", "teal", "pink"];
 var underScores = [];
 var wins = 0;
 var lives = " " + 6;
-var guessed = [];
 var wrongLetter = [];
 var userGuesses = [];
-var randomWord;
-var winCounter = 0;
+var randomWord = [];
+var correct = 0;
+var img = new Image();
+var div = document.getElementById("hmImage")
 // randomly select a word from the list
 
 function starterGame(){
+    underScores = [];
     randomWord = colors[Math.floor(Math.random() * colors.length)];
     console.log('random Word =' + randomWord);
     
@@ -21,22 +23,32 @@ function starterGame(){
 
     document.getElementById("currWord").innerHTML = underScores.join(" ");
     console.log(underScores);
-    wrongLetter = []
+    wrongLetter = [];
     lives = " " + 6;
-
+    userGuesses = [];
+    correct = 0;
+    
+    document.getElementById("guessed").innerHTML = " " + " " + wrongLetter.join(" ");
     document.getElementById("mylives").innerHTML = lives;
 }
 
 function win()
 {
-    if (winCounter === randomWord.length)
+    if(correct === randomWord.length)
     {
-        alert("Winner");
+        wins++;
+        document.getElementById("winner").style.display = "inline-block";
     }
 }
 
+function goback()
+{   
+    document.getElementById("winner").style.display = "none";
+    document.getElementById("loser").style.display = "none";
+    starterGame();
+}
 
-document.onkeyup = function(event)
+document.onkeyup = function (event)
 {
     userGuesses = event.key;
     
@@ -47,24 +59,30 @@ document.onkeyup = function(event)
         if(randomWord[i] === userGuesses)
         {
             underScores[i] = userGuesses;
-            console.log(underScores);
-            winCounter++;
+            document.getElementById("currWord").innerHTML = underScores.join(" ");
+            correct++;        
             win();
-        }
-        else
-        {
-           
         }
      }
     }
     else
     {
      wrongLetter.push(userGuesses);
-     lives--;
+     console.log(wrongLetter)
+     if(lives > 1)
+     {
+        lives--;
+     }
+     else
+     {
+        document.getElementById("loser").style.display = "inline-block";
+     }
      document.getElementById("mylives").innerHTML = " " + " " + lives;
+     document.getElementById("guessed").innerHTML = " " + " " + wrongLetter.join(" ");
     }
 }
 
+document.getElementById("mywins").innerHTML = " " + " " + wins;
 starterGame();
 
 // show the empty spaces for each letter with underscores
